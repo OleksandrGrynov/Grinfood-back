3c0b760f40cd53e81143ebe50787053ea8198ea3
OleksandrGrynov <alex170707228@gmail.com>
2025-05-06 15:13:36 +0300

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-module.exports = (controller) => {
-    const router = express.Router();
-
-    router.get('/:uid', controller.getUserName.bind(controller));
-
-    return router;
-};
